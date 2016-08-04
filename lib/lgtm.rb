4349bd2aed6e887bd9fa29bd8272aac469a1d3be
HaiTo <kimura@sansan.com>
2016-08-04 16:57:56 +0900

    using /tmp and return lgtm struct

--- lib/lgtm.rb
+++ lib/lgtm.rb
@@ -12,13 +12,15 @@ module Lgtm
 
     LAST_EXECUTED_AT_TIME_PATH = 'last_executed_at.time'.freeze
     CACHED_LINKS_PATH = 'cached_links.links'.freeze
-    BASE_PATH = File.expand_path('..', __FILE__)
+    BASE_PATH = '/tmp'
 
     LINK_PAR_PAGE_BASE = 32.freeze
 
     LGTM_MARKDOWN = '[![LGTM](__LGTM_IMAGE_DETAILS_PATH__)](__LGTM_IMAGE_PATH__)'
     LGTM_MARKDOWN_DETAIL_PATH_BASE = 'http://lgtm.in/p/'.freeze
     LGTM_MARKDOWN_IMAGE_PATH_BASE = 'http://lgtm.in/i/'.freeze
+
+    LgtmLinkStruct = Struct.new(:link, :image)
     
     def not_set_valiavle_alert
       print NOT_SET_VALIAVLE_ALERT_1
@@ -37,24 +39,29 @@ module Lgtm
       end
     end
 
-    def fetch_lgtm_link_markdown!
-      links =
+    def fetch_lgtm_link_markdown!(mode: :link)
+      link_structs =
         if Time.now.to_i - @last_executed_at.to_i > REFLASH_TIME
           update_executed_at!
 
-          links = fetch_links
-          cache_links!(links)
+          link_structs = fetch_links
+          cache_links!(link_structs)
 
-          links
+          link_structs
         else
           @cached_links.nil? ? fetch_links : Marshal.load(@cached_links)
         end
 
-      link = links.sample
-
-      hash = link.split('/').last
-
-      LGTM_MARKDOWN.gsub('__LGTM_IMAGE_DETAILS_PATH__', "#{LGTM_MARKDOWN_DETAIL_PATH_BASE + hash}").gsub('__LGTM_IMAGE_PATH__', "#{LGTM_MARKDOWN_IMAGE_PATH_BASE + hash}")
+      link_struct = link_structs.compact.sample
+    
+      case mode
+      when :link
+        link_hash = link_struct.link.split('/').last
+        
+        LGTM_MARKDOWN.gsub('__LGTM_IMAGE_DETAILS_PATH__', "#{LGTM_MARKDOWN_DETAIL_PATH_BASE + link_hash}").gsub('__LGTM_IMAGE_PATH__', "#{LGTM_MARKDOWN_IMAGE_PATH_BASE + link_hash}")
+      when :image
+        link_struct.image
+      end
     end
 
     private
@@ -69,7 +76,13 @@ module Lgtm
 
     def fetch_link_per_page!(cursor, links, agent)
       page = agent.get(@lgtm_my_page + "?page=#{cursor}")
-      links << page.links.map(&:href).select {|link| link.match(LGTM_LINK_BASE) }
+      # links << page.links.map(&:href).select {|link| link.match(LGTM_LINK_BASE) }
+      
+      links << page.links.map {|link|
+        next unless link.href.match(LGTM_LINK_BASE)
+        
+        LgtmLinkStruct.new(link.href, link.node.children[1].attributes['src'].value)
+      }
 
       if links.flatten!.size % LINK_PAR_PAGE_BASE == 0
         fetch_link_per_page!(cursor + 1, links, agent)
