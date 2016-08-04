4d92b1303841c9c98752a2df7f9e0dc12e2fec57
HaiTo <kimura@sansan.com>
2016-08-04 18:01:52 +0900

    configrable lgtm index

--- lgtm.gemspec
+++ lgtm.gemspec
@@ -4,23 +4,23 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'lgtm/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "lgtm"
+  spec.name          = 'lgtm'
   spec.version       = Lgtm::VERSION
-  spec.authors       = ["HaiTo"]
-  spec.email         = ["kimura@sansan.com"]
+  spec.authors       = ['HaiTo']
+  spec.email         = ['kimura@sansan.com']
 
   spec.summary       = 'fetch image url in lgtm.in'
   spec.description   = 'fetch image url in lgtm.in to pbcopy'
   spec.homepage      = 'https://github.com/haito/lgtm'
 
-  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
-  spec.bindir        = "exe"
-  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.files         = `git ls-files -z`.split("\x0").reject {|f| f.match(%r{^(test|spec|features)/}) }
+  spec.bindir        = 'exe'
+  spec.executables   = spec.files.grep(%r{^exe/}) {|f| File.basename(f) }
+  spec.require_paths = ['lib']
 
-  spec.add_development_dependency "bundler", "~> 1.12"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency 'bundler', '~> 1.12'
+  spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'rspec', '~> 3.0'
   spec.add_development_dependency 'pry'
   spec.add_development_dependency 'pry-byebug'
   spec.add_dependency 'mechanize'
--- lib/lgtm.rb
+++ lib/lgtm.rb
@@ -1,27 +1,31 @@
-require "lgtm/version"
+require 'lgtm/version'
 require 'mechanize'
 
 module Lgtm
+  class Fixnum
+    MAX = 4_611_686_018_427_387_903
+  end
+
   class In
     LGTM_LINK_BASE = 'http://lgtm.in/i'.freeze
-    NOT_SET_VALIAVLE_ALERT_1 = "please set enviromant variable LGTM_IN_USER_PATH \n"
-    NOT_SET_VALIAVLE_ALERT_2 = "lgtm.in mypage example ->  http://lgtm.in/l/HaiTo \n"
-    NOT_SET_VALIAVLE_ALERT_3 = "in .bashrc, .zshrc -> export LGTM_IN_USER_PATH='http://lgtm.in/l/HaiTo' \n"
+    NOT_SET_VALIAVLE_ALERT_1 = "please set enviromant variable LGTM_IN_USER_PATH \n".freeze
+    NOT_SET_VALIAVLE_ALERT_2 = "lgtm.in mypage example ->  http://lgtm.in/l/HaiTo \n".freeze
+    NOT_SET_VALIAVLE_ALERT_3 = "in .bashrc, .zshrc -> export LGTM_IN_USER_PATH='http://lgtm.in/l/HaiTo' \n".freeze
 
-    REFLASH_TIME = 604_800.freeze
+    REFLASH_TIME = 604_800
 
     LAST_EXECUTED_AT_TIME_PATH = 'last_executed_at.time'.freeze
     CACHED_LINKS_PATH = 'cached_links.links'.freeze
-    BASE_PATH = '/tmp'
+    BASE_PATH = '/tmp'.freeze
 
-    LINK_PAR_PAGE_BASE = 32.freeze
+    LINK_PAR_PAGE_BASE = 32
 
-    LGTM_MARKDOWN = '[![LGTM](__LGTM_IMAGE_DETAILS_PATH__)](__LGTM_IMAGE_PATH__)'
+    LGTM_MARKDOWN = '[![LGTM](__LGTM_IMAGE_DETAILS_PATH__)](__LGTM_IMAGE_PATH__)'.freeze
     LGTM_MARKDOWN_DETAIL_PATH_BASE = 'http://lgtm.in/p/'.freeze
     LGTM_MARKDOWN_IMAGE_PATH_BASE = 'http://lgtm.in/i/'.freeze
 
-    LgtmLinkStruct = Struct.new(:link, :image)
-    
+    LgtmLinkStruct = Struct.new(:index, :link, :image)
+
     def not_set_valiavle_alert
       print NOT_SET_VALIAVLE_ALERT_1
       print NOT_SET_VALIAVLE_ALERT_2
@@ -39,7 +43,7 @@ module Lgtm
       end
     end
 
-    def fetch_lgtm_link_markdown!(mode: :link)
+    def fetch_lgtm_link_markdown!(mode, picker)
       link_structs =
         if Time.now.to_i - @last_executed_at.to_i > REFLASH_TIME
           update_executed_at!
@@ -52,13 +56,24 @@ module Lgtm
           @cached_links.nil? ? fetch_links : Marshal.load(@cached_links)
         end
 
-      link_struct = link_structs.compact.sample
-    
+      link_struct =
+        case picker.to_i
+        when 1..Fixnum::MAX
+          link_structs.compact[picker.to_i]
+        else
+          link_structs.compact.sample
+        end
+
+      unless link_struct
+        print "you could set to NUMBER at between 0 and #{link_structs.size}"
+        exit(1)
+      end
+
       case mode
       when :link
         link_hash = link_struct.link.split('/').last
-        
-        LGTM_MARKDOWN.gsub('__LGTM_IMAGE_DETAILS_PATH__', "#{LGTM_MARKDOWN_DETAIL_PATH_BASE + link_hash}").gsub('__LGTM_IMAGE_PATH__', "#{LGTM_MARKDOWN_IMAGE_PATH_BASE + link_hash}")
+
+        LGTM_MARKDOWN.gsub('__LGTM_IMAGE_DETAILS_PATH__', (LGTM_MARKDOWN_DETAIL_PATH_BASE + link_hash).to_s).gsub('__LGTM_IMAGE_PATH__', (LGTM_MARKDOWN_IMAGE_PATH_BASE + link_hash).to_s)
       when :image
         link_struct.image
       end
@@ -76,12 +91,9 @@ module Lgtm
 
     def fetch_link_per_page!(cursor, links, agent)
       page = agent.get(@lgtm_my_page + "?page=#{cursor}")
-      # links << page.links.map(&:href).select {|link| link.match(LGTM_LINK_BASE) }
-      
-      links << page.links.map {|link|
-        next unless link.href.match(LGTM_LINK_BASE)
-        
-        LgtmLinkStruct.new(link.href, link.node.children[1].attributes['src'].value)
+
+      links << page.links.select {|link| link.href.match(LGTM_LINK_BASE) }.map.with_index(links.size + 1) {|link, index|
+        LgtmLinkStruct.new(index, link.href, link.node.children[1].attributes['src'].value)
       }
 
       if links.flatten!.size % LINK_PAR_PAGE_BASE == 0
@@ -97,6 +109,12 @@ module Lgtm
 
     def cache_links!(links)
       File.open(BASE_PATH + '/' + CACHED_LINKS_PATH, 'w') {|f| f.write(Marshal.dump(links)) }
+    end
+
+    def load_cached_links!
+      Marshal.load(@cached_links)
+    rescue
+      fetch_links
     end
   end
 end
